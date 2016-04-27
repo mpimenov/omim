9da581df277f08e7e071b96e281e5578944fa511
Maxim Pimenov <m@maps.me>
2016-04-27 18:31:39 +0300

    Now types are also returned along with category names.

--- editor/new_feature_categories.hpp
+++ editor/new_feature_categories.hpp
@@ -23,7 +23,7 @@ public:
   NewFeatureCategories(NewFeatureCategories && other)
     : m_index(move(other.m_index))
     , m_types(move(other.m_types))
-    , m_categoryNames(move(other.m_categoryNames))
+    , m_categoriesByLang(move(other.m_categoriesByLang))
   {
   }
 
@@ -35,19 +35,20 @@ public:
   // can be applied to a newly added feature.
   void AddLanguage(string const & lang);
 
-  // Returns names (in language |lang|) of categories that have a synonym containing
+  // Returns names (in language |lang|) and types of categories that have a synonym containing
   // the substring |query| (in any language that was added before).
   // The returned list is sorted.
-  vector<string> Search(string const & query, string const & lang) const;
+  vector<pair<string, uint32_t>> Search(string const & query, string const & lang) const;
 
-  // Returns all registered names of categories in language |lang|.
+  // Returns all registered names of categories in language |lang| and
+  // types corresponding to these names.
   // The returned list is sorted.
-  vector<string> GetAllCategoryNames(string const & lang);
+  vector<pair<string, uint32_t>> GetAllCategoryNames(string const & lang);
 
 private:
   indexer::CategoriesIndex m_index;
   vector<uint32_t> m_types;
-  map<string, vector<string>> m_categoryNames;
+  map<string, vector<pair<string, uint32_t>>> m_categoriesByLang;
 
   DISALLOW_COPY(NewFeatureCategories);
 };
